19fe41ba33f51b1f7559f96a1b7bb0f33fc991ef
Cihan <cihansercaliskan@gmail.com>
2020-02-29 22:38:04 +0300

    CubeRunner game added

---
+++ Cube Runner/Cube Runner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    bool GameHasEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject CompleteLevelUI;
+   public void EndGame()
+    {
+
+        if(GameHasEnded == false)
+        {
+            Debug.Log("Game Over!");
+            GameHasEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    
+    }
+    public void CompleteLevel()
+    {
+        Debug.Log("Level Won!");
+        CompleteLevelUI.SetActive(true);
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+   
+}
