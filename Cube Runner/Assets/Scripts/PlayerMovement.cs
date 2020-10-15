90d67888b726664aba70be7f342f3e9e684311ce
Cihan <50696201+CihanserC@users.noreply.github.com>
2020-10-15 20:58:45 +0300

    Cube Runner to Car Drift

--- Cube Runner/Assets/Scripts/PlayerMovement.cs
+++ Cube Runner/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,50 @@ public class PlayerMovement : MonoBehaviour
 
     public float forwardforce = 5000f;
 
-    public float sidewayforce = 100f;
+    public float sidewayforce = 80f;
+
+    public bool LeftDir  = false;
+    public bool RightDir = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GoLeftButton()
+    {
+        LeftDir = true;
+        //rb.AddForce(-sidewayforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    public void ReverseLeftButton() // This is important, when user put up his finger from the button it stops the force applied on the player
+    {
+        LeftDir = false;
+        //rb.AddForce(-sidewayforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+
+    public void GoRightButton()
+    {
+        RightDir = true;
+    }
+
+    public void ReverseRightButton()
+    {
+        RightDir = false;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        //***************** For the keyboard***********
+
         rb.AddForce(0,0, forwardforce * Time.deltaTime);
 
         if(Input.GetKey("d"))
@@ -29,5 +68,17 @@ public class PlayerMovement : MonoBehaviour
             FindObjectOfType<GameManager>().EndGame();
         }
 
+        //***************** For the virtual buttons***********
+
+        if(LeftDir == true)
+        {
+            rb.AddForce(-sidewayforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
+        if (RightDir == true)
+        {
+            rb.AddForce(sidewayforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+        }
     }
 }
